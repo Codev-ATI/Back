1a5c484752e273379dfd9f7213209442afd9a88f
abruhat <alexandre.bruhat@etu.univ-lyon1.fr>
2019-11-08 11:46:04 +0100

    :rocket: First commit

---
+++ src/main/kotlin/fr/codev/projectk/ProjectkApplication.kt
@@ -0,0 +1,11 @@
+package fr.codev.projectk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectkApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectkApplication>(*args)
+}
---
+++ src/test/kotlin/fr/codev/projectk/ProjectkApplicationTests.kt
@@ -0,0 +1,13 @@
+package fr.codev.projectk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
